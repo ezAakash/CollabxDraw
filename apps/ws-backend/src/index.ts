e5a582f9cbee115bf03ff99988d4ddf311b87a7f
ezAakash <oenmazoro@gmail.com>
2026-03-24 16:56:01 +0530

    B: Add express server ( signin, login, createRoom endpoint ) and Add ws server logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,91 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from 'ws'
+import jwt from "jsonwebtoken"
 
-const wss = new WebSocketServer( { port: 8080 })
+const ws = new WebSocketServer({ port: 8080 })
 
-wss.on("connection", (socket) => {
-    socket.send("connected to the network")
+const allSockets = new Map<string, WebSocket[]>();
+
+// webSocket only takes either string or binary.
+let currentUserRoom: string = "";
+
+ws.on("connection", (socket, request) => {
+
+    const url = request.url;
+    if (!url) {
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1])
+    const token = queryParams.get('token') || ""
+
+    const response = jwt.verify(token, JWT_TOKEN) //env variable signature)
+
+    if (!response ) {
+        socket.close()
+        return;
+    }
+
+    
+
+
+     try {
+            const response = jwt.verify(token, process.env.JWT_SECRET!) as { id: String }
+        
+            if (!response) {
+                return res.status(403).json({
+                    message: "Incorrect Credentials"
+                })
+            }
+
+            req.userId = response.id
+            next()
+        }
+        catch (e) {
+                res.status(403).json({ message: "Invalid or expired token"})
+        }
+    
+    socket.on("message", (message) => {
+        // string to an object 
+        try{  
+            //@ts-ignore
+            const parsedMessage = JSON.parse(message);
+            if (parsedMessage.type === "join") {
+    
+                currentUserRoom = parsedMessage.payload.roomId;
+    
+                if (!allSockets.has(currentUserRoom)) {
+                    allSockets.set(currentUserRoom, [])
+                }
+    
+                allSockets.get(currentUserRoom)!.push(socket)
+                socket.send("Room Joined")   
+            }
+    
+            if (parsedMessage.type === "chat") {
+                let targetRoom = parsedMessage.payload.roomId;
+                let sockets = allSockets.get(targetRoom);
+                
+                if(sockets) {
+                    sockets.forEach((soc) => {
+                        if (soc.readyState === WebSocket.OPEN){
+                            soc.send(parsedMessage.payload.message)
+                        }
+                    })
+                }
+            }       
+
+        }
+        catch(err) {
+            socket.send("You sent a invalid input")
+        }
+    })
+
+    socket.on("close", () => {
+        if (currentUserRoom) {
+            const sockets = allSockets.get(currentUserRoom)
+            if (sockets) {
+                allSockets.set(currentUserRoom, sockets.filter((soc) => soc !== socket))
+            }
+        }
+    })
+
 })
